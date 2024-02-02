09aef5bfc0b3d2f0c5b44698fdb67c45f7bca2f8
ksvvReddy <vardhanreddy071094@gmail.com>
2024-02-02 15:56:32 +0530

    first commit

---
+++ src/test/java/util/Config.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+	private static final String DEFAULT_PROPERTIES = "config/default.properties";
+	private static Properties properties;
+
+	public static void initialize() throws IOException {
+
+		// load default properties
+		properties = readPropertiesFile(DEFAULT_PROPERTIES);
+
+		// check for any override
+		for (String key : properties.stringPropertyNames()) {
+			if (System.getProperties().containsKey(key)) {
+				properties.setProperty(key, System.getProperty(key));
+			}
+		}
+
+	}
+
+	public static String get(String key) {
+		return properties.getProperty(key);
+	}
+
+	public static Properties readPropertiesFile(String fileName) throws IOException {
+		FileInputStream fis = null;
+		Properties prop = null;
+		try {
+			fis = new FileInputStream(fileName);
+			prop = new Properties();
+			prop.load(fis);
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			fis.close();
+		}
+		return prop;
+	}
+}
